f1566c91fec3893002f1ab6b57eac9d7b903ab8f
Martin Andersen <martin.andersn@gmail.com>
2019-10-07 21:38:01 +0200

    Update config.js

--- content/meta/config.js
+++ content/meta/config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteTitle: "Ørn Forlag", // <title>
   shortSiteTitle: "Ørn Forlag", // <title> ending for posts and pages
   siteDescription: "Utgiver av bøker og innhold til mobiltelefoner og GPSer, om norsk natur og kultur.",
-  siteUrl: "ornforlag.netlify.com",
-  // pathPrefix: "",
+  siteUrl: "https://ornforlag.netlify.com/",
+   pathPrefix: "/",
   siteImage: "preview.jpg",
   siteLanguage: "nb",
 
